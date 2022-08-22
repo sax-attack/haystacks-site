6735b3db82ba309c3e9490d7516b4cd821605b3a
LatusDenis <d.lateezy@gmail.com>
2022-08-23 01:00:35 +0400

    chore: add favicon

--- src/pages/sfr-solutions/portfolio-tabs/portfolio-tabs.jsx
+++ src/pages/sfr-solutions/portfolio-tabs/portfolio-tabs.jsx
@@ -14,12 +14,13 @@ export const PortfolioTabs = ({ currentTab, changeTab }) => {
     <div className='flex w-full mx-auto mb-9'>
       {tabs.map((tab, index) =>
         isBiggerSm ? (
-          <button className='flex flex-col items-center w-full' onClick={changeTab(index)}>
+          <button key={index} className='flex flex-col items-center w-full' onClick={changeTab(index)}>
             <img src={CubeIcon} alt='cube' className={c('w-12 h-12 mb-4', currentTab !== index && 'opacity-0')} />
             <p className={c('font-rubik text-5xl transition-opacity', currentTab !== index && 'opacity-10')}>{tab.text}</p>
           </button>
         ) : (
           <button
+            key={index}
             className={`flex flex-col items-center w-full text-xs p-1 m-1 text-typography-light60 ${
               currentTab !== index ? '' : 'border-[1px] font-rubik border-[0.5px] rounded-[62px] !text-typography-light'
             }`}
