fbe1915c0779377b80ace4ee9120383414c1e206
Daria Cherniavska <dariaachernyavskaya@gmail.com>
2022-08-22 18:58:36 +0300

    updated bg and videos

--- src/pages/market-solutions/market-solutions.jsx
+++ src/pages/market-solutions/market-solutions.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import { useEffect } from 'react';
 import { Card, RequestForm } from '../../common';
 import { routes } from '../../router';
-import CubeIcon from '../../assets/cube-icon.svg'
+import { useWindowDimensions } from '../../hooks';
+import { constants } from '../../constants';
+import CubeIcon from '../../assets/cube-icon.svg';
 
 const data = [
   {
@@ -23,30 +26,67 @@ const MarketSolutions = () => {
     document.title = routes.market.title;
   }, []);
 
-  return ( 
+  const { width } = useWindowDimensions();
+
+  const isBiggerSm = width > constants.breakpoints.sm;
+
+  return (
     <div>
-      <section className="section">
-        <div className="container">
-          <div className="flex flex-col items-center">
-            <img src={CubeIcon} alt="cube" className="w-12 h-12 mb-12" />
-            <h1 className="font-rubik text-lg md:text-6xl text-center max-w-xl">Lens Predictive Market Intelligence</h1>
-            <p className="text-sm md:text-2xl font-light text-center mt-12">Utilizing our proprietary machine learning models, we run multitudes <br /> of regression analyses to surface data points predicting market change.</p>
+      <section className='section'>
+        <div className='container'>
+          <div className='flex flex-col items-center'>
+            <img src={CubeIcon} alt='cube' className='w-12 h-12 mb-12' />
+            <h1 className='font-rubik text-lg md:text-6xl text-center max-w-xl'>Lens Predictive Market Intelligence</h1>
+            <p className='text-sm md:text-2xl font-light text-center mt-12'>
+              Utilizing our proprietary machine learning models, we run multitudes <br /> of regression analyses to surface data
+              points predicting market change.
+            </p>
           </div>
 
-          <div className="flex flex-col md:flex-row justify-between mt-24">
+          <div className='flex flex-col md:flex-row justify-between mt-24'>
             {data.map((item) => (
-              <Card key={item.heading} subheading={item.subheading} text={item.text} className="w-full md:w-2/6 mb-8 md:mb-0 mr-8">
+              <Card
+                key={item.heading}
+                subheading={item.subheading}
+                text={item.text}
+                className='w-full md:w-2/6 mb-8 md:mb-0 mr-8'
+              >
                 <Card.Heading>{item.heading}</Card.Heading>
-                <Card.Text className="mt-5">{item.text}</Card.Text>
+                <Card.Text className='mt-5'>{item.text}</Card.Text>
               </Card>
             ))}
           </div>
         </div>
       </section>
 
+      <div className='flex flex-col items-center px-24 pb-8 mt-20 md:pb-16 text-center'>
+        <img src={CubeIcon} alt='cube' className='w-12 h-12 mb-4' />
+        <p className='text-sm md:text-2xl font-extralight'>
+          Customize the intelligent heatmap by weighting the metrics to uncover matching micro-markets.
+        </p>
+      </div>
+
+      <div className='max-w-[1200px] mx-auto'>
+        {isBiggerSm ? (
+          <iframe
+            src='https://drive.google.com/file/d/1709VnSYfaDxyWoA5QrGExD67E4rASz1G/preview'
+            width='100%'
+            height='480'
+            allow='autoplay'
+          ></iframe>
+        ) : (
+          <iframe
+            src='https://drive.google.com/file/d/171dOgCKFimyTRFFqs7zGT1cb3_w_vsHQ/preview'
+            width='100%'
+            height='480'
+            allow='autoplay'
+          ></iframe>
+        )}
+      </div>
+
       <RequestForm />
     </div>
-  )
+  );
 };
 
 export { MarketSolutions };
